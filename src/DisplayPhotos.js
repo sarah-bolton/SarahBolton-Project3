4eb0873b1b943bda16442b4298496a70580ffa6c
SarahBolton <bolton.l.sarah@gmail.com>
2022-08-16 22:45:04 -0400

    alt tag added

--- src/DisplayPhotos.js
+++ src/DisplayPhotos.js
@@ -1,9 +1,9 @@
 function DisplayPhotos({ photos }) {
-    
+
     return (
         <section className="background-img">
             {photos.length === 0 ? (
-                <img src="https://images.unsplash.com/photo-1615149598868-8bc923532ee6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="bgimg"></img>
+                <img src="https://images.unsplash.com/photo-1615149598868-8bc923532ee6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="bgimg" alt="an older camera"></img>
             ) : (
                 <>
                     <div className="photos">
